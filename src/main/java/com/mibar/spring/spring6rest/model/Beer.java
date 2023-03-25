3e07e57636c2f73e24c0edf276693e3542ef5b1b
Miguel Ibarra <mibar0005@gmail.com>
2023-03-25 10:50:34 -0400

    base project

---
+++ src/main/java/com/mibar/spring/spring6rest/model/Beer.java
@@ -0,0 +1,4 @@
+package com.mibar.spring.spring6rest.model;
+
+public class Beer {
+}
---
+++ src/test/java/com/mibar/spring/spring6rest/Spring6RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mibar.spring.spring6rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
